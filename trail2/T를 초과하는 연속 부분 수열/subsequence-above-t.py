49052ba03bfd567e048e11432141c589beb6926a
Gyobeom <45328273+Gyobeom@users.noreply.github.com>
2026-06-11 22:52:31 +0900

    [Passed] T를 초과하는 연속 부분 수열 (49ms, 16MB)

---
+++ trail2/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -0,0 +1,22 @@
+n, t = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+def solution():
+    max = 0
+    cnt = 0
+    for i in range(len(arr)):
+        if i == 0:
+            if arr[i] > t:
+                cnt += 1
+        elif arr[i] > t:
+            cnt += 1
+        else:
+            if max < cnt:
+                max = cnt
+            cnt = 0
+    if max < cnt:
+        max = cnt
+    return max
+
+print(solution())
