b45edbc50b2d7b7cd3e3cf0c29c3dfece48a70b3
Gyobeom <45328273+Gyobeom@users.noreply.github.com>
2026-06-19 19:00:02 +0900

    [Passed] 격자 위의 편안한 상태 (121ms, 20MB)

--- trail2/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ trail2/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,37 +1,35 @@
-n, m = map(int, input().split())
-points = [tuple(map(int, input().split())) for _ in range(m)]
-
-# Please write your code here.
-# 색칠할 칸에서 동,서,남,북으로 범위 안에 있고, 색칠이 이미 되어 있는 칸이 3개인 경우 편안한 상태
+import sys
 
 def solution():
-    #빈 칸 만들기
-    board = [[0 for _ in range(n+1)] for _ in range(m+1)]
-    dxs, dys = [0,1,0,-1], [1,0,-1,0]
-    cnt = 0
+    # n: 격자 크기(N x N), m: 색칠 횟수
+    line1 = sys.stdin.readline().split()
+    if not line1: return
+    n, m = map(int, line1)
     
-    def in_range(x,y):
-        return 0 <= x and x < n and 0 <= y and y < m
+    # N x N 격자이므로, 열의 크기도 n+1이어야 합니다.
+    board = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+    
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+    
+    def in_range(x, y):
+        return 1 <= x <= n and 1 <= y <= n
 
-    def check_board(x,y):
+    def check_board(x, y):
         cnt = 0
         for i in range(4):
             nx, ny = x + dxs[i], y + dys[i]
-            if in_range(nx,ny) and board[nx][ny] == 1:
+            if in_range(nx, ny) and board[nx][ny] == 1:
                 cnt += 1
-        if cnt == 3:
-            return True
-        return False
+        return cnt == 3
     
-    # -1 필요 배열에 맞추기 위해서
-    # 먼저 색칠 후 주변 판단. 
-    for x,y in points:
-        board[x-1][y-1] = 1
-        flexable_check = check_board(x-1,y-1)
-        if flexable_check:
+    for _ in range(m):
+        x, y = map(int, sys.stdin.readline().split())
+        board[x][y] = 1
+        
+        # 방금 색칠한 칸이 편안한지 확인
+        if check_board(x, y):
             print(1)
         else:
             print(0)
-solution()
 
-    
+solution()
